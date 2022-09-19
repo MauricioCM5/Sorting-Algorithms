6a1826626582a2e5bc6e7b77eb5f51d9cfa793c2
Mauricio Colque <maumc123@outlook.com>
2022-09-19 11:01:14 -0500

    Update mergesort.h

--- Right_Algorithms/mergesort.h
+++ Right_Algorithms/mergesort.h
@@ -1,28 +1,4 @@
-#pragma once
-/*int* merge(int a[], int b[]){
 
-        int sA = sizeA(a), sB = sizeA(b);
-        int sizeC = sA + sB;
-        int* c = new int(sizeC);
-        //[sizeC];
-        int counter = 0, ma = 0, mb = 0, i;
-
-        while(ma < sA && mb < sB){
-                if(a[ma] < b[mb]){
-                        c[counter] = a[ma];
-                        ma++;
-                }
-                else{
-                        c[counter] = b[mb];
-                        mb++;
-                }
-                counter++;
-                cout << "HERE\n";
-        }
-        for(i = ma; i < sA; i++, counter++) c[counter] = a[ma];
-        for(i = mb; i < sB; i++, counter++) c[counter] = b[mb];
-        return c;
-}*/
 
 void merge(int c[], int ini, int fin, int mid) {
 
@@ -57,30 +33,9 @@ void merge(int c[], int ini, int fin, int mid) {
     delete[] b;
 }
 
-// Continously split the array in half
-/*int* mergeSort(int array[], int n){
-        if (n == 1) return array;
-
-        int* left = new int(ceil(n/2));
-        int* right = new int(n/2);
-        int i, fh = ceil(n/2);
-        for(i = 0; i < fh; i++)
-                left[i] = array[i];
-
-        for(i = 0; i < n/2; i++)
-                right[i] = array[i+(n/2)];
-
-        cout << "HERE\n";
-
-        left = mergeSort(left, fh);
-        right = mergeSort(right, n/2);
-        merge(left, right);
-
-
-}*/
 
 void mergeSort(int array[], int ini, int fin) {
-    // cout << "HERE\n";
+    
     if (ini >= fin)
         return;
     int mid = ini + (fin - ini) / 2;
